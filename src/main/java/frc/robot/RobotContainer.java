b5a8169be4766960b4393f1b9a238c0ffbc68678
rachitkakkar <rachitkakkar7@gmail.com>
2025-01-25 14:08:02 -0600

    Reformat Drive Code and Add Cameras

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,36 +12,21 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.drivetrain.DriveSubsystem;
 
 public class RobotContainer {
-    private final CommandXboxController joystick = new CommandXboxController(0);
+  private final CommandXboxController joystick = new CommandXboxController(0);
 
-    private final Telemetry logger = new Telemetry(Constants.Drive.MAX_SPEED.in(MetersPerSecond));
+  private final Telemetry logger = new Telemetry(Constants.Drive.MAX_SPEED.in(MetersPerSecond));
 
-    private final DriveSubsystem DRIVE_SUBSYSTEM = new DriveSubsystem(new DriveSubsystem.Hardware(), logger);
+  private final DriveSubsystem DRIVE_SUBSYSTEM = new DriveSubsystem(DriveSubsystem.initializeHardware(), logger);
 
-    public RobotContainer() {
-        configureBindings();
-    }
+  public RobotContainer() {
+    configureBindings();
+  }
 
-    private void configureBindings() {
-        DRIVE_SUBSYSTEM.bindControls(() -> -joystick.getLeftY(), () -> -joystick.getLeftX(), () -> -joystick.getRightX());
+  private void configureBindings() {
+    DRIVE_SUBSYSTEM.bindControls(() -> -joystick.getLeftY(), () -> -joystick.getLeftX(), () -> -joystick.getRightX());
+  }
 
-        // joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        // joystick.b().whileTrue(drivetrain.applyRequest(() ->
-        //     point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        // ));
-
-        // // Run SysId routines when holding back/start and X/Y.
-        // // Note that each routine should be run exactly once in a single log.
-        // joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-        // joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-        // joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-        // joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
-
-        // // reset the field-centric heading on left bumper press
-        // joystick.leftBumper().onTrue(drivetrain.runOnce(() -> drivetrain.seedFieldCentric()));
-    }
-
-    public Command getAutonomousCommand() {
-        return Commands.print("No autonomous command configured");
-    }
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
 }
--- src/main/java/frc/robot/subsystems/drivetrain/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/drivetrain/DriveSubsystem.java
@@ -2,8 +2,13 @@ package frc.robot.subsystems.drivetrain;
 
 import java.util.function.DoubleSupplier;
 
+import org.lasarobotics.hardware.revrobotics.Spark.MotorKind;
+import org.lasarobotics.vision.AprilTagCamera;
+
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
+
+import edu.wpi.first.apriltag.AprilTagFields;
 import frc.robot.Constants;
 import frc.robot.Telemetry;
 import frc.robot.generated.TunerConstants;
@@ -11,64 +16,104 @@ import frc.robot.subsystems.StateMachine;
 import frc.robot.subsystems.SystemState;
 
 public class DriveSubsystem extends StateMachine implements AutoCloseable {
-    public static record Hardware() {
-    }
+  public static record Hardware(
+    AprilTagCamera frontLeftCamera,
+    AprilTagCamera frontRightCamera,
+    AprilTagCamera rearCamera
+  ) {}
 
-    public enum State implements SystemState {
-        DRIVER_CONTROL {
-            @Override
-            public void initialize() {
-            }
+  public enum State implements SystemState {
+    DRIVER_CONTROL {
+      @Override
+      public void initialize() {
+      }
 
-            @Override
-            public void execute() {
-                s_drivetrain.setControl(
-                    s_drive.withVelocityX(Constants.Drive.MAX_SPEED.times(-s_driveRequest.getAsDouble())) // Drive forward with negative Y (forward)
-                        .withVelocityY(Constants.Drive.MAX_SPEED.times(-s_strafeRequest.getAsDouble())) // Drive left with negative X (left)
-                        .withRotationalRate(Constants.Drive.MAX_ANGULAR_RATE.times(-s_rotateRequest.getAsDouble())) // Drive counterclockwise with negative X (left)
-                );
-            }
+      @Override
+      public void execute() {
+        s_drivetrain.setControl(
+          s_drive.withVelocityX(Constants.Drive.MAX_SPEED.times(-s_driveRequest.getAsDouble())) // Drive forward with negative Y (forward)
+            .withVelocityY(Constants.Drive.MAX_SPEED.times(-s_strafeRequest.getAsDouble())) // Drive left with negative X (left)
+            .withRotationalRate(Constants.Drive.MAX_ANGULAR_RATE.times(-s_rotateRequest.getAsDouble())) // Drive counterclockwise with negative X (left)
+        );
+      }
 
-            @Override
-            public State nextState() {
-                return this; // no escape
-            }
-        },
-    }
+      @Override
+      public State nextState() {
+        return this; // no escape
+      }
+    },
+  }
 
-    private static CommandSwerveDrivetrain s_drivetrain;
-    private static SwerveRequest.FieldCentric s_drive;
-    
-    private static DoubleSupplier s_driveRequest = () -> 0;
-    private static DoubleSupplier s_strafeRequest = () -> 0;
-    private static DoubleSupplier s_rotateRequest = () -> 0;
+  private static CommandSwerveDrivetrain s_drivetrain;
+  private static SwerveRequest.FieldCentric s_drive;
 
-    private static Telemetry s_logger;
+  private static DoubleSupplier s_driveRequest = () -> 0;
+  private static DoubleSupplier s_strafeRequest = () -> 0;
+  private static DoubleSupplier s_rotateRequest = () -> 0;
 
-    public DriveSubsystem(Hardware driveHardware, Telemetry logger) {
-        super(State.DRIVER_CONTROL);
+  private static Telemetry s_logger;
 
-        s_drivetrain = TunerConstants.createDrivetrain();
+  public DriveSubsystem(Hardware driveHardware, Telemetry logger) {
+    super(State.DRIVER_CONTROL);
 
-        /* Setting up bindings for necessary control of the swerve drive platform */
-        s_drive = new SwerveRequest.FieldCentric()
-            .withDeadband(Constants.Drive.MAX_SPEED.times(0.1))
-            .withRotationalDeadband(Constants.Drive.MAX_ANGULAR_RATE.times(0.1)) // Add a 10% deadband
-            .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
+    s_drivetrain = TunerConstants.createDrivetrain();
 
-        s_logger = logger;
-        s_drivetrain.registerTelemetry(logger::telemeterize);
-    }
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    s_drive = new SwerveRequest.FieldCentric()
+      .withDeadband(Constants.Drive.MAX_SPEED.times(0.1))
+      .withRotationalDeadband(Constants.Drive.MAX_ANGULAR_RATE.times(0.1)) // Add a 10% deadband
+      .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
 
-    public void bindControls(DoubleSupplier driveRequest, DoubleSupplier strafeRequest, DoubleSupplier rotateRequest) {
-        s_driveRequest = driveRequest;
-        s_strafeRequest = strafeRequest;
-        s_rotateRequest = rotateRequest;
-    }
+    s_logger = logger;
+    s_drivetrain.registerTelemetry(logger::telemeterize);
+  }
 
-    @Override
-    public void close() throws Exception {
-        s_drivetrain.close();
-    }
+  /**
+   * Initialize hardware devices for drive subsystem
+   *
+   * @return Hardware object containing all necessary devices for this subsystem
+   */
+  public static Hardware initializeHardware() {
+    AprilTagCamera frontLeftCamera = new AprilTagCamera(
+      Constants.VisionHardware.CAMERA_A_NAME,
+      Constants.VisionHardware.CAMERA_A_LOCATION,
+      Constants.VisionHardware.CAMERA_A_RESOLUTION,
+      Constants.VisionHardware.CAMERA_A_FOV,
+      AprilTagFields.k2025Reefscape.loadAprilTagLayoutField()
+    );
 
+    AprilTagCamera frontRightCamera = new AprilTagCamera(
+      Constants.VisionHardware.CAMERA_B_NAME,
+      Constants.VisionHardware.CAMERA_B_LOCATION,
+      Constants.VisionHardware.CAMERA_B_RESOLUTION,
+      Constants.VisionHardware.CAMERA_B_FOV,
+      AprilTagFields.k2025Reefscape.loadAprilTagLayoutField()
+    );
+
+    AprilTagCamera rearCamera = new AprilTagCamera(
+      Constants.VisionHardware.CAMERA_C_NAME,
+      Constants.VisionHardware.CAMERA_C_LOCATION,
+      Constants.VisionHardware.CAMERA_C_RESOLUTION,
+      Constants.VisionHardware.CAMERA_C_FOV,
+      AprilTagFields.k2025Reefscape.loadAprilTagLayoutField()
+    );
+
+    Hardware driveHardware = new Hardware(
+      frontLeftCamera,
+      frontRightCamera,
+      rearCamera
+    );
+    return driveHardware;
+  }
+
+  public void bindControls(DoubleSupplier driveRequest, DoubleSupplier strafeRequest, DoubleSupplier rotateRequest) {
+    s_driveRequest = driveRequest;
+    s_strafeRequest = strafeRequest;
+    s_rotateRequest = rotateRequest;
+  }
+
+  @Override
+  public void close() throws Exception {
+    s_drivetrain.close();
+  }
 }
