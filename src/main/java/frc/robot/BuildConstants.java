ebaeea6512e1869b1240aad30f400f76f9e81ca3
rachitkakkar <rachitkakkar7@gmail.com>
2025-01-29 18:16:20 -0600

    Updoot

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "PH2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 23;
-  public static final String GIT_SHA = "0866b0bb17927ea745a4ed83cce85fbadb986ab9";
-  public static final String GIT_DATE = "2025-01-27 21:13:47 CST";
-  public static final String GIT_BRANCH = "develop";
-  public static final String BUILD_DATE = "2025-01-27 21:14:37 CST";
-  public static final long BUILD_UNIX_TIME = 1738034077092L;
-  public static final int DIRTY = 1;
+  public static final int GIT_REVISION = 25;
+  public static final String GIT_SHA = "b6842d9de4ae7f1333b197d066fb5a2eefecbce5";
+  public static final String GIT_DATE = "2025-01-29 17:22:49 CST";
+  public static final String GIT_BRANCH = "endeffecter";
+  public static final String BUILD_DATE = "2025-01-29 18:16:08 CST";
+  public static final long BUILD_UNIX_TIME = 1738196168376L;
+  public static final int DIRTY = 0;
 
   private BuildConstants(){}
 }
