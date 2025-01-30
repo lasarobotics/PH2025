6ef255eb01e4ee997f4c9ebb85bec9c79cedc2fc
arunvaradarajan <104042443+woopers6@users.noreply.github.com>
2025-01-30 17:16:27 -0600

    update the code to use rio instead of canivore

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,8 +37,8 @@ public final class Constants {
   }
 
   public static class LiftHardware {
-   public static final TalonFX.ID ELEVATOR_MOTOR_ID = new TalonFX.ID("LiftHardware/Elevator", PhoenixCANBus.CANIVORE, 4);
-   public static final TalonFX.ID PIVOT_MOTOR_ID = new TalonFX.ID("LiftHardware/Pivot", PhoenixCANBus.CANIVORE, 5);
+   public static final TalonFX.ID ELEVATOR_MOTOR_ID = new TalonFX.ID("LiftHardware/Elevator", PhoenixCANBus.RIO, 4);
+   public static final TalonFX.ID PIVOT_MOTOR_ID = new TalonFX.ID("LiftHardware/Pivot", PhoenixCANBus.RIO, 5);
    public static final Distance SPROCKET_PITCH_RADIUS = Inches.of((1.751)/(2.0));
    public static final LimitSwitch.ID ELEVATOR_HOMING_BEAM_BREAK_PORT = new LimitSwitch.ID("LiftHardware/HomingSwitch", 0);
   }
