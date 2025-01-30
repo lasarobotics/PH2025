84ff10c7a6a27c65b54fa04d62e5c1a5d1b11929
David-Dube <david.dubelv@yahoo.com>
2025-01-29 18:32:00 -0600

    make auto alignment rotation take the shortest path

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "PH2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "42890d718be4189ebac1d8df087bf80afd74a0b5";
-  public static final String GIT_DATE = "2025-01-27 18:42:16 CST";
-  public static final String GIT_BRANCH = "HEAD";
-  public static final String BUILD_DATE = "2025-01-27 18:54:20 CST";
-  public static final long BUILD_UNIX_TIME = 1738025660633L;
+  public static final int GIT_REVISION = 23;
+  public static final String GIT_SHA = "163c297351bccefff486005c0e556957afd00deb";
+  public static final String GIT_DATE = "2025-01-27 19:00:36 CST";
+  public static final String GIT_BRANCH = "drivetrain_alignment";
+  public static final String BUILD_DATE = "2025-01-29 18:26:19 CST";
+  public static final long BUILD_UNIX_TIME = 1738196779084L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
