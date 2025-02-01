9433f9daa3042c6a8a1e6fd422e81dd657753916
David-Dube <david.dubelv@yahoo.com>
2025-02-01 09:37:53 -0600

    Add x and y alignment

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,13 +7,13 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "PH2025";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 28;
-  public static final String GIT_SHA = "84ff10c7a6a27c65b54fa04d62e5c1a5d1b11929";
-  public static final String GIT_DATE = "2025-01-29 18:41:24 CST";
+  public static final int GIT_REVISION = 33;
+  public static final String GIT_SHA = "78065e65efaec1c47905b8a3ce5e9aefabc429e9";
+  public static final String GIT_DATE = "2025-01-29 19:12:59 CST";
   public static final String GIT_BRANCH = "drivetrain_alignment";
-  public static final String BUILD_DATE = "2025-01-29 18:41:44 CST";
-  public static final long BUILD_UNIX_TIME = 1738197704360L;
-  public static final int DIRTY = 0;
+  public static final String BUILD_DATE = "2025-02-01 09:30:35 CST";
+  public static final long BUILD_UNIX_TIME = 1738423835792L;
+  public static final int DIRTY = 1;
 
   private BuildConstants(){}
 }
