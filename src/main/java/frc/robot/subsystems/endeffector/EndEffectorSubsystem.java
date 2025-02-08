af0cbd734adf7c606afc5c1a0b880ac979089192
David-Dube <david.dubelv@yahoo.com>
2025-02-08 12:45:20 -0600

    Add more logging

--- src/main/java/frc/robot/subsystems/endeffector/EndEffectorSubsystem.java
+++ src/main/java/frc/robot/subsystems/endeffector/EndEffectorSubsystem.java
@@ -296,6 +296,7 @@ public class EndEffectorSubsystem extends StateMachine implements AutoCloseable 
     super.periodic();
 
     Logger.recordOutput(getName() + "/state", getState().toString());
+    Logger.recordOutput(getName() + "/isCoralCentered", isCoralCentered());
   }
 
   public void close() {
